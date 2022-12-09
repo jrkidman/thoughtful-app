6e3e561dffd03d61a89701aa3fea4887467bcfd4
Jill Kidman <jillkidman@gmail.com>
2022-12-09 14:18:05 -0600

    updated material ui

--- src/components/layoutComponents/Header.js
+++ src/components/layoutComponents/Header.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import { Avatar } from '@mui/material';
